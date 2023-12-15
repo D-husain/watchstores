9822d1aedc0ab00d06743a7188d441978ff61b9c
jitesh-joshi <jitesh.joshi2412@gmail.com>
2023-12-15 11:37:23 +0530

    brand filter changes checkbox

--- src/main/resources/static/ajax/shop/brandfilter.js
+++ src/main/resources/static/ajax/shop/brandfilter.js
@@ -1,39 +1,74 @@
 $(document).ready(function() {
-	$.ajax({
-		url: '/brand/data',
-		method: 'GET',
-		dataType: 'json',
-		success: function(response) {
-			if (response) {
-				var brandFilter = $('#brandFilter');
-				response.forEach(function(brand) {
-					var brandListItem = '<li class="sidebar-menu-collapse-list">' +
-						'<div class="accordion">' +
-						'<button onclick="filterByBrand(\'' + brand.brand + '\')">' + brand.brand + '</button>' +
-						'</div>' +
-						'</li>';
-					brandFilter.append(brandListItem);
-				});
-			}
-		},
-		error: function(xhr, status, error) {
-			console.error(error);
-		}
-	});
+    // Function to fetch brand data and populate checkboxes
+    function fetchBrandData() {
+        $.ajax({
+            url: '/brand/data',
+            method: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                if (response && response.length > 0) {
+					var productsLength = response.length;
+                    var brandFilter = $('#brandFilter');
+                    var previousCheckbox = null; // To keep track of the previously checked checkbox
+                    response.forEach(function(brand, index) {
+                        var brandListItem = $('<li></li>');
+                        var checkboxId = 'brandCheckbox_' + index;
+                        var label = $('<label class="checkbox-default" for="' + checkboxId + '"></label>');
+                        var input = $('<input type="checkbox" id="' + checkboxId + '">');
+                        var span = $('<span>' + brand.brand + ' (' + productsLength + ')</span>');
+
+                        label.append(input);
+                        label.append(span);
+                        brandListItem.append(label);
+                        brandFilter.append(brandListItem);
+
+                        // Event listener for checkbox change
+                        input.on('change', function() {
+                            if (this.checked) {
+                                // If another checkbox was previously checked, uncheck it
+                                if (previousCheckbox && previousCheckbox !== this) {
+                                    $(previousCheckbox).prop('checked', false);
+                                }
+                                filterByBrand(brand.brand);
+                                previousCheckbox = this;
+                            } else {
+                                // Clear filtered products if unchecked
+                                getAllProducts();
+                                //clearFilteredProducts();
+                            }
+                        });
+                    });
+                } else {
+                    console.error('Empty or invalid response received from the server.');
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('AJAX request failed:', error);
+                // Handle error scenario here, e.g., show an error message to the user
+            }
+        });
+    }
+
+    // Rest of your code (filterByBrand function, displayFilteredProducts, displayListProducts, clearFilteredProducts)
+
+    // Call function to fetch brand data and populate checkboxes
+    fetchBrandData();
 });
 
-function filterByBrand(brand) {
-	$.ajax({
-		url: 'http://localhost:8081/api/shop?brand=' + brand,
-		method: 'GET',
-		dataType: 'json',
-		success: function(response) {
-			// On successful response, clear existing products and display filtered products
-			displayFilteredProducts(response);
-			displayListProducts(response);
-		},
-		error: function(xhr, status, error) {
-			console.error(error);
-		}
-	});
-}
+
+    // Function to filter products by brand
+    function filterByBrand(brand) {
+        $.ajax({
+            url: 'http://localhost:8081/api/shop?brand=' + brand,
+            method: 'GET',
+            dataType: 'json',
+            success: function(response) {
+                // On successful response, clear existing products and display filtered products
+                displayFilteredProducts(response);
+                displayListProducts(response);
+            },
+            error: function(xhr, status, error) {
+                console.error(error);
+            }
+        });
+    }
